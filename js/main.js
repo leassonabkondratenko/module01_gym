3eea1a6af16608afe01e7cbf1a9411302897b792
leassonabkondratenko <98252843+leassonabkondratenko@users.noreply.github.com>
2022-01-27 03:28:13 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let now = new Date().toLocaleDateString();
+console.log("Сегодня " + now);
+
+// Button Up
+
+window.onscroll = function(){scrollFunction()};
+
+const upbuttons = document.querySelectorAll(".button-up");
+
+for (const upbutton of upbuttons) {
+    upbutton.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+});
+}
+
+function scrollFunction(){
+    if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+        document.getElementById('btnUp').className = 'button-up visible';
+    } else {
+        document.getElementById('btnUp').className = 'button-up hidden';
+    }
+}
